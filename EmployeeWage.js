9083a36fe6c0c06888cd7832a7c125d93acc3f3b
“Love <opera.prem@gmail.com>
2022-09-16 21:37:24 +0530

    Added : Calculating Wages for a Month assuming 20 Working Days in a Month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -34,7 +34,7 @@ switch (empCheck) {
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage : "+ empWage);*/
 
-console.log("------------------UC-2--AND--UC-3-----------------------");
+/*console.log("------------------UC-2--AND--UC-3-----------------------");
 // Calculating Daily Employee Wage based on part time or full time work...........
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -58,4 +58,33 @@ let empHrs = 0;
 let empCheck = Math.floor(Math.random() *3);
 empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage : "+ empWage);
+console.log("Emp Wage : "+ empWage);*/
+
+console.log("------------------UC-4-----------------------");
+// Calculating Wages for a Month assuming 20 Working Days in a Month..........
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+
+function getWorkingHours(empCheck){
+switch (empCheck) {
+    case IS_PART_TIME:
+        return PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        return FULL_TIME_HOURS;
+        break;
+    default:
+        return 0;
+}
+}
+let empHrs = 0;
+for(let day = 0; day <NUM_OF_WORKING_DAYS; day++){
+let empCheck = Math.floor(Math.random() *3);
+empHrs += getWorkingHours(empCheck);
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hours : "+ empHrs +"\nEmp Wage : "+ empWage);
