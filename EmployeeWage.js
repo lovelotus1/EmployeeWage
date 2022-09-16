526eb30454f9f0b372493a75b8d647f2e929438d
“Love <opera.prem@gmail.com>
2022-09-16 22:00:13 +0530

    Added : Method To Store Daily Wage In Array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -89,7 +89,7 @@ empHrs += getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Total Hours : "+ empHrs +"\nEmp Wage : "+ empWage);*/
 
-console.log("------------------UC-5-----------------------");
+/*console.log("------------------UC-5-----------------------");
 // Calculating Wages for a Month assuming 20 Working Days in a Month..........
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -119,4 +119,44 @@ while(empHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
     empHrs += getWorkingHours(empCheck);
 }
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total working days : "+ totalWorkingDays +"\nTotal Hours : "+ empHrs +"\nEmp Wage : "+ empWage);
+console.log("Total working days : "+ totalWorkingDays +"\nTotal Hours : "+ empHrs +"\nEmp Wage : "+ empWage);*/
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+let empDailyWageArray = new Array();
+
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
+
+function getWorkingHours(empCheck){
+    switch (empCheck) 
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
+    }
+}
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+{
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() *3);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArray.push(empHrs)
+}
+// Calculating Wages for a Month assuming 20 Working Days in a Month..........
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total working days : "+ totalWorkingDays +"\nTotal Hours : "+ totalEmpHrs +"\nEmp Wage : "+ empWage);
