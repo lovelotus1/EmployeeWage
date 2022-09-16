3f26b44c784927d023531b1ec4d09925478bddc4
“Love <opera.prem@gmail.com>
2022-09-16 21:15:19 +0530

    Added : Check Employee Presence

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,12 @@
 console.log("Welcome to Employee Wage Computation");
+console.log("------------------UC-1----------------------------");
+const IS_PRESENT=0;
+let attendance=Math.floor(Math.random() * 2);
+if(attendance==IS_PRESENT)
+{
+    console.log("Employee is Present");
+}
+else
+{
+    console.log("Employee is Absent");
+}
